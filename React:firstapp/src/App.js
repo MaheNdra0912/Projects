e237812151d33cac9de3943ee757b81ef2be86a7
MaheNdra0912 <mahendrasai0901@gmail.com>
2025-02-11 12:11:55 -0700

    Added Few Projects

---
+++ React:firstapp/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function Header() {
+  return (
+    <header>
+      <h1 className='App-header'>My First React App</h1>
+    </header>
+  );
+}
+
+function App() {
+  return (
+   <Header />
+  );
+}
+
+export default App;
